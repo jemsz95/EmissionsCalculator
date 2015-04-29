a294143a460418136cc53a4c6fa8dcd4e4e796ca
Javier Meza <javenmeza@gmail.com>
2015-04-29 18:11:22 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+(function() {
+    var app = angular.module('emisionsCalculator');
+
+    app.controller('calculatorController', function($scope) {
+
+    });
+
+    app.controller('SectionsCtlr', function($scope) {
+        this.calculate = 
+    });
+})();
