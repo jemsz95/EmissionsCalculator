d927c5e6500f9caeaae91f739f5cd92e1e8dfa0a
Javier Meza <javenmeza@gmail.com>
2015-04-30 04:23:52 -0500

    WORK IN PROGRESS (UNUSABLE), check details
    - Question templates: Done! These have angular directives that dynamically adapt them
    - JSON template: Done! It seems that the JSON template is complete! It might (probably will)
                     be shortened in the future
    - HOMEPAGE: (ALMOST) Done! Final adjustments needed (like inserting the question template)
    - Base Javascript: In progress! This is the core and it still is a work in progress.
    - Custom directive: TBA! This will(maybe) happen in the future
    - Translations: TBA! This project's text is completely in spanish(on the user end). Although
                    the code is written in english I have planned international support but I
                    really don't know when it (if ever) will happen.
    -

--- js/app.js
+++ js/app.js
@@ -2,10 +2,57 @@
     var app = angular.module('emisionsCalculator');
 
     app.controller('calculatorController', function($scope) {
+        $scope.calculate = function() {
 
+        }
     });
 
     app.controller('SectionsCtlr', function($scope) {
-        this.calculate = 
+        $scope.questionModifiers = {};
+        $scope.questionUpdates = 0;
+        $scope.isDirty = false;
+
+        $scope.calculate = function() {
+            if($scope.isDirty) {
+
+            }
+        }
+
+        $scope.on("questionCalculationComplete", function(e, wasDirty) {
+            if(wasDirty) {
+                $scope.isDirty = true;
+            }
+
+            questionUpdates++;
+
+            if(questionUpdates == $scope.questions.length) {
+                $scope.calculate();
+                questionUpdates = 0;
+            }
+        });
     });
+
+
+    app.controller('QuestionCtlr', function($scope) {
+        $scope.isDirty = false;
+        $scope.answer = 0;
+        $scope.questionExec = new Function('gVars', 'answer', $scope.question.exec)
+
+        $scope.calculate = function() {
+
+            if($scope.isDirty) {
+                $scope.result = $scope.questionExec($scope.$parent.questionModifiers, $scope.answer);
+            }
+
+            $scope.emit("questionCalculationComplete", $scope.isDirty)
+
+            $scope.isDirty = false;
+        }
+
+        $scope.setDirty() {
+            $scope.isDirty = true;
+        }
+    });
+
+
 })();
